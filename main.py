1e67c6afba68eec133f2462f2f9e6ecba8cd2d40
DoseiBrayin <ardilabrayan68@gmail.com>
2024-04-02 23:42:40 -0500

    (CONFIG) Cors Config

--- main.py
+++ main.py
@@ -2,8 +2,28 @@
 from fastapi import FastAPI
 from models import user_model
 from controller import user_controller
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Lista de orígenes permitidos
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:5173",
+]
+
+# Agrega el middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Aquí estamos definiendo las rutas de nuestra API
 @app.get("/items/{item_id}", response_model=user_model.Item)
 def read_item(item_id: int):
